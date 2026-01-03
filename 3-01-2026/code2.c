dd9fe25a9e2a1d6058c39b858fe06f1d496583d4
Jaswanth Chennamsetty <jaswanthch24924@gmail.com>
2026-01-03 21:49:53 +0530

    Add files via upload

---
+++ 3-01-2026/code2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+    int num;
+    printf("Enter an Integer:");
+    scanf("%d",&num);
+    if(num==0)
+    {
+        printf("Zero ");
+
+    }
+    else if("num>0")
+    {
+        printf("Positive Number");
+
+    }
+    else
+    {
+        printf("Negetive Number");
+    }
+  
+    return 0;
+}
