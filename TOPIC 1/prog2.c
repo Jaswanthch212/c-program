ba036c7f41a413f8efdb1ee5d173ff1aa98577e5
Jaswanth Chennamsetty <jaswanthch24924@gmail.com>
2026-02-11 00:01:22 +0530

    Add files via upload

---
+++ TOPIC 1/prog2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    char name[100];
+    printf("ENTER YOUR NAME:");
+    scanf("%s",&name);
+    printf("%s",name);
+
+return 0;
+}
---
+++ TOPIC 1/prog7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    char a[100];
+    int b;
+    printf("ENTER A STRING & AN INEGER:");
+
+    scanf("%s %d",&a,&b);
+    printf("%s  %d",a,b);
+
+return 0;
+}
