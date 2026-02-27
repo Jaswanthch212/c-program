5416ecf9b4e29c676cbb12e5b01fecf4058e0cd1
Jaswanth Chennamsetty <jaswanthch24924@gmail.com>
2026-02-27 21:30:42 +0530

    Add files via upload

--- FOR LOOP 2/tickets.c
+++ FOR LOOP 2/tickets.c
@@ -11,7 +11,7 @@ printf("Total Collection = %d",sum);
 
     return 0;
 }
-/*SAN.KJFBQW?oioRHF;I 3QEWHRIW3hoiu3ti3irliulI23GRL3WIHEFKJWEGHLIUGH34LIURH;I3HF;Owhr;oih54y[y rbpytp9854yuul5rhgul ohlkrhrljhjkhjhjjhjhjhjhjhjklfdhflkhdoifhdfdjifkhdffdhkjlfkldjfjklorhj/xlkhj/olrdjfhkfdjj/lkhdj/ljb/lizdrfjbglk/dfjbklgeseiIhgoiehoihgoiah?OIO ptqwoto/hwth ohoiehi/fhgeoithgt/qowHRL<>Jawbr,a]
+
 
 
 
